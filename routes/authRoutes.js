9b19029c8765c604ea8a7c51bac20543a4e191a7
neyoungandy <>
2025-06-08 23:23:00 +0100

    Fix User model import path for Render deployment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
-const User = require("../models/User");
+const User = require("../models/User.js");  
+
 
 const router = express.Router();
 
